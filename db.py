12a404b156f957e833fc9ac1856fd397b171a299
matthew-l-craig <matt@matthewlcraig.net>
2024-11-13 08:52:39 -0500

    add database

---
+++ db.py
@@ -0,0 +1,19 @@
+import sqlite3
+import os
+
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect('remo.db')
+        self.cur = self.conn.cursor()
+        self._init_db()
+
+    def _init_db(self):
+        files = os.listdir('sql')
+        for file in files:
+            with open(f'sql/{file}', 'r') as f:
+                query = f.read()
+                self.cur.execute(query)
+
+    def query(self, query):
+        self.cur.execute(query)
+        self.conn.commit()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from db import Database
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-
+db = Database()
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
