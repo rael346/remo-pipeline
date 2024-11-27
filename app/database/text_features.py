94e97206ee6971ba47e7272b0267be1c48633af8
Matthew Craig <matthewleocr@gmail.com>
2024-11-26 21:03:25 -0500

    add text_features and remove unnecessary imports

--- app/database/text_features.py
+++ app/database/text_features.py
@@ -1,7 +1,7 @@
 from sqlmodel import Field, SQLModel
 
 
-class SubGenres(SQLModel, table=True):
+class TextFeatures(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True, nullable=False)
     name: int = Field(nullable=False)
     isbn: str = Field(max_digits=13, nullable=False)
