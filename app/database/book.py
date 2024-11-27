6cb7d563a441d2ac644e76a38348baadf0db3945
Matthew Craig <matthewleocr@gmail.com>
2024-11-26 20:59:30 -0500

    remove todo

--- app/database/book.py
+++ app/database/book.py
@@ -3,7 +3,6 @@ from sqlmodel import Field, SQLModel
 
 
 class Book(SQLModel, table=True):
-    # TODO: finish adding fields to this table
     id: int | None = Field(default=None, primary_key=True, nullable=False)
     isbn: int = Field(max_digits=13, unique=True, nullable=False)
     title: str = Field(nullable=False)
